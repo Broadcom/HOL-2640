833b2732a610b295176800480d49dfdd283d69a0
William Nead <william.nead@broadcom.com>
2026-02-26 09:25:36 -0500

    Update prepare-pwsh.sh
    updates

--- lab-startup/prepare-pwsh.sh
+++ lab-startup/prepare-pwsh.sh
@@ -4,6 +4,12 @@
 #The PW variable requires the file listed to be present as it is on the manager VM.
 #This script will not work on the LMC
 
+#Accounting for Git problems in base template
+rm -f .git/index.lock
+git stash
+git pull --force
+
+#Copy files from Git repo on Manager to be used on Console in HOL environment
 vPodPW=$(</home/holuser/creds.txt) 
 #Script to build VPC environment for Lab2 Module1
 sshpass -p $vPodPW scp -o StrictHostKeyChecking=no /vpodrepo/2026-labs/2640/lab-startup/DeployVPC.ps1 holuser@10.1.10.130:/home/holuser/labfiles/hol-2640-02/DeployVPC.ps1
